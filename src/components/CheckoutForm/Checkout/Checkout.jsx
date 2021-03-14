1adbf0caeed205e225ddcc9ea2e311b0cf9b2a04
zaky aly <zaky3ly@gmail.com>
2021-03-14 17:41:18 +0100

    adding checkout component

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {
+  CssBaseline,
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
+
+import { commerce } from "../../../lib/commerce";
+import useStyles from "./styles";
+import AddressForm from "../AddressForm";
+import PaymentForm from "../PaymentForm";
+
+const steps = ["Shipping address", "Payment details"];
+
+const Checkout = ({ cart }) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
+
+  const Confirmation = () => <div>Confirmation</div>;
+
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
+  );
+};
+
+export default Checkout;
